767d995166b235c0427d456ae0f2d9498a4488fc
Michaelpkr <michael.parker@ramseysolutions.com>
2022-04-27 19:27:26 -0500

    Added NoaArrayChallenge.js

---
+++ NoaArrayChallenge.js
@@ -0,0 +1,21 @@
+const users = [
+    "Michael",
+    "Heather",
+    "Ben",
+    "Savannah",
+    "Miguel",
+    "Noa"
+];
+ 
+// Given an array of users, write a function that goes through the array and checks to see if a specific user exists in the array
+// inputs:
+// String => name of user
+// Array => array of users to be checked
+ 
+// output:
+//Boolean => true or false
+ 
+function isUserPresent(firstName, listOfUsers) {
+    // write code below
+ 
+}
